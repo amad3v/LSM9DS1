bf57d89e04847a93a4a8423a87af1aa56ef2c6f4
amad3v <amad3v@gmail.com>
2022-01-27 03:15:59 +0000

    fix: remove unused header.

--- src/mag.h
+++ src/mag.h
@@ -27,7 +27,6 @@
 #define __LIB__LSM9DS1_MAG_H__
 
 #include <Arduino.h>
-#include <Streaming.h>
 #include <Wire.h>
 #include "helpers.h"
 #include "setting.h"
@@ -39,89 +38,85 @@
  * @class Mag
  * @brief M unit.
  */
-class Mag: public Unit {
-  public:
-    /**
-     * @brief Construct a new Mag object
-     *
-     * @param i2c_ptr Pointer to #I2C.
-     * @param m_range M's range (full scale range).
-     * @param m_rate M's rate (Output data rate).
-     * @param m_mode M's operating mode.
-     * @param addr M's address.
-     */
-    Mag(I2C* i2c_ptr,
-        mag::FSR m_range,
-        mag::ODR m_rate,
-        mag::OP_MODE m_mode,
-        uint8_t addr = mag::ADDRESS);
+class Mag : public Unit {
+public:
+  /**
+   * @brief Construct a new Mag object
+   *
+   * @param i2c_ptr Pointer to #I2C.
+   * @param m_range M's range (full scale range).
+   * @param m_rate M's rate (Output data rate).
+   * @param m_mode M's operating mode.
+   * @param addr M's address.
+   */
+  Mag(I2C *i2c_ptr, mag::FSR m_range, mag::ODR m_rate, mag::OP_MODE m_mode, uint8_t addr = mag::ADDRESS);
 
-    /**
-     * @brief Check if M's ID is valid.
-     *
-     * @return true if valid,
-     * @return false otherwise.
-     */
-    bool valid_device_id();
+  /**
+   * @brief Check if M's ID is valid.
+   *
+   * @return true if valid,
+   * @return false otherwise.
+   */
+  bool valid_device_id();
 
-    /**
-     * @brief Compute M's current values, in @f$Gauss@f$.
-     *
-     * @param data Array to hold values.
-     * @return true if read,
-     * @return false otherwise.
-     */
-    bool read_m(float data[]);
+  /**
+   * @brief Compute M's current values, in @f$Gauss@f$.
+   *
+   * @param data Array to hold values.
+   * @return true if read,
+   * @return false otherwise.
+   */
+  bool read_m(float data[]);
 
-    /**
-     * @brief Reset M.
-     *
-     * @param config Configuration.
-     * @return true if reset,
-     * @return false otherwise.
-     */
-    bool reset(uint8_t config = mag::DEFAULT_CONFIG);
+  /**
+   * @brief Reset M.
+   *
+   * @param config Configuration.
+   * @return true if reset,
+   * @return false otherwise.
+   */
+  bool reset(uint8_t config = mag::DEFAULT_CONFIG);
 
-    /**
-     * @brief Setup M.
-     *
-     * @return true if set,
-     * @return false otherwise.
-     */
-    bool setup();
+  /**
+   * @brief Setup M.
+   *
+   * @return true if set,
+   * @return false otherwise.
+   */
+  bool setup();
 
-    /**
-     * @brief Check for new values.
-     *
-     * @return true if new values available.
-     * @return false otherwise.
-     */
-    bool available();
+  /**
+   * @brief Check for new values.
+   *
+   * @return true if new values available.
+   * @return false otherwise.
+   */
+  bool available();
 
-  private:
-    /**
-     * @brief Full scale range.
-     */
-    mag::FSR fsr;
+private:
+  /**
+   * @brief Full scale range.
+   */
+  mag::FSR fsr;
 
-    /**
-     * @brief Output data rate.
-     */
-    mag::ODR odr;
+  /**
+   * @brief Output data rate.
+   */
+  mag::ODR odr;
 
-    /**
-     * @brief Operating mode.
-     */
-    mag::OP_MODE mode;
+  /**
+   * @brief Operating mode.
+   */
+  mag::OP_MODE mode;
 
-    /**
-     * @brief Retrieve raw data.
-     *
-     * @param data Array to hold values.
-     * @return true if retrieved,
-     * @return false otherwise
-     */
-    bool raw_m(int16_t data[]);
+  /**
+   * @brief Retrieve raw data.
+   *
+   * @param data Array to hold values.
+   * @return true if retrieved,
+   * @return false otherwise
+   */
+  bool raw_m(int16_t data[]);
 };
 
 #endif /* __LIB__LSM9DS1_MAG_H__ */
